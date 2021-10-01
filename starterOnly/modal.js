4f9812843983ddaf56d38c8294214d678a0f7b03
guillaume gaillard <gaillard.guillaum@gmail.com>
2021-10-01 16:24:00 +0200

    close fixed

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,3 +1,31 @@
+
+// DOM Elements
+const modalBg = document.querySelector(".bground");
+const content = document.querySelector(".content")
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+const closeBg = document.querySelector('.close')
+
+//timer function 
+function timeout(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// launch modal form
+const launchModal = () => {
+  modalBg.style.display = "block";
+}
+// launch modal event
+const closeModal = async() => {
+  content.classList.add("modal-close-animation" )
+  modalBg.style.background = 'rgba(26, 39, 156, 0)';
+ await timeout(795)
+  modalBg.style.display = "none";
+  modalBg.style.background= "rgba(26, 39, 156, 0.4)";
+  content.classList.remove("modal-close-animation" )
+}
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+closeBg.addEventListener("click" , closeModal)
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -6,18 +34,4 @@ function editNav() {
     x.className = "topnav";
   }
 }
-
-// DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-// launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
-}
-
 
